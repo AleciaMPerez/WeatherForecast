f0425674eb87c091003aaf00fc562ff46fedd7e4
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-20 17:47:18 -0400

    pseudocode in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Search for a city
+
+//That city's current date, temperature, an icon representation of weather conditions, the humidity, and the the wind speed appear on the screen.
+
+//The 5 day forecast appears in individual cards.
+
+//List searched cities
