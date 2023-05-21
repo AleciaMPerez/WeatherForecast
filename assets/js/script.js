75a3bdffa6cf849b5252b7d87574905ae0216d06
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-21 14:08:22 -0400

    global elements created for current

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+//Global elements
+var currentDayEl = document.querySelector(.dayCurrent)
+var currentDateEl = document.querySelector(.dateCurrent)
+var currentIconEl = document.querySelector(.iconCurrent)
+var currentTempEl = document.querySelector(.temperatureCurrent)
+var currentHumidityEl = document.querySelector(.humidityCurrent)
+var currentWindSpeedEl = document.querySelector(.windSpeedCurrent)
+var apiKey = "0094105c713bc17695af1d675f234ed7"
+
+
 //Search for a city
 
 //That city's current date, temperature, an icon representation of weather conditions, the humidity, and the the wind speed appear on the screen.
