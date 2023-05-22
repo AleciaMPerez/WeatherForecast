bbd560c8d8ccb26e38bf81a9b5230751e4eb9c6b
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-22 15:36:51 -0400

    corrected api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,27 @@
 //Global elements
-var apiKey = "0094105c713bc17695af1d675f234ed7"
+var apiKey = "0094105c713bc17695af1d675f234ed7";
 //var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey
-var search = document.querySelector("#searchButton")
-var cityInput= document.querySelector("#city")
-
+var search = document.querySelector("#searchButton");
+var cityInput = document.querySelector("#city");
 
 //Search for a city
 
 function searchCity(e) {
-    e.preventDefault();
-    var apiGeo = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value + "&appid=" + apiKey
-    fetch(apiGeo)
-    .then(function (response)
-    {return response.json()
+  e.preventDefault();
+  var apiGeo =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityInput.value +
+    "&appid=" +
+    apiKey + "&units=imperial";
+  fetch(apiGeo)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(data){
-        console.log(data)
-        console.log(data[0].local_names.es)
-    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
-search.addEventListener("click", searchCity)
-
-
-
+search.addEventListener("click", searchCity);
 
 //That city's current date, temperature, an icon representation of weather conditions, the humidity, and the the wind speed appear on the screen.
 
