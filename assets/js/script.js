c13732b27249ca122366f4414851f68578429301
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-22 14:10:27 -0400

    Search City

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
 //Global elements
-var currentDayEl = document.querySelector(.dayCurrent)
-var currentDateEl = document.querySelector(.dateCurrent)
-var currentIconEl = document.querySelector(.iconCurrent)
-var currentTempEl = document.querySelector(.temperatureCurrent)
-var currentHumidityEl = document.querySelector(.humidityCurrent)
-var currentWindSpeedEl = document.querySelector(.windSpeedCurrent)
 var apiKey = "0094105c713bc17695af1d675f234ed7"
+//var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey
+var search = document.querySelector("#searchButton")
+var cityInput= document.querySelector("#city")
 
 
 //Search for a city
+
+function searchCity(e) {
+    e.preventDefault();
+    var apiGeo = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value + "&appid=" + apiKey
+    fetch(apiGeo)
+    .then(function (response)
+    {return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        console.log(data[0].local_names.es)
+    })
+}
+search.addEventListener("click", searchCity)
+
+
+
 
 //That city's current date, temperature, an icon representation of weather conditions, the humidity, and the the wind speed appear on the screen.
 
