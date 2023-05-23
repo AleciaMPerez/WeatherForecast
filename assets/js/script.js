9fc58faff3c4cefe1883a996652f64205956de0f
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-23 17:58:07 -0400

    created borders and styling in CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 //Global elements
 var apiKey = "0094105c713bc17695af1d675f234ed7";
-//var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey
 var search = document.querySelector("#searchButton");
 var cityInput = document.querySelector("#city");
 
@@ -8,12 +7,12 @@ var cityInput = document.querySelector("#city");
 
 function searchCity(e) {
   e.preventDefault();
-  var apiGeo =
+  var apiURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityInput.value +
     "&appid=" +
     apiKey + "&units=imperial";
-  fetch(apiGeo)
+  fetch(apiURL)
     .then(function (response) {
       return response.json();
     })
@@ -24,6 +23,10 @@ function searchCity(e) {
 search.addEventListener("click", searchCity);
 
 //That city's current date, temperature, an icon representation of weather conditions, the humidity, and the the wind speed appear on the screen.
+
+
+
+
 
 //The 5 day forecast appears in individual cards.
 
