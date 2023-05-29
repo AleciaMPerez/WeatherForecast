f22bf05d5eb81fa4c49b7385cd6191178d64e8c7
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-29 13:18:37 -0400

    displayCurrent function

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,19 @@ search.addEventListener("click", searchCity);
 //That city's current date, temperature, an icon representation of weather conditions, the humidity, and the the wind speed appear on the screen.
 
 function displayCurrent (data) {
+//api
+    var apiURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityInput.value +
+    "&appid=" +
+    apiKey + "&units=imperial";
+    fetch (apiUrl)
+    .then (function(response)
+    {return response.json()
+    })
+    .then( response(data)
+    {console.log(data)
+    })
     //current temperature
     var currentTemp = data.main.temperature;
     var currentTempDiv = document.querySelector(".temperatureCurrent");
@@ -34,17 +47,14 @@ function displayCurrent (data) {
     //current wind speed
     var currentWind = data.wind.speed;
     var currentWindDiv = document.querySelector(".windSpeedCurrent");
-    //api
-    var apiURL =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-    cityInput.value +
-    "&appid=" +
-    apiKey + "&units=imperial";
+    link.textContent=data[i].
 }
 
 
 
 
 //The 5 day forecast appears in individual cards.
+ var apiUrl= "api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=" +apiKey
+
 
 //List searched cities
